1ba3bccf6632032cc92a691ebab1f8abd577ec30
EdViTeR <maxpivnenko@mail.ru>
2023-01-16 15:24:53 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!-- canIuse -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mogo</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+	<header class="header">
+		<div class="container">
+			<div class="header__inner">
+				<div class="header__logo">MoGo</div>
+
+				<nav class="nav">
+					<a class="nav__link active" href="#">About</a>
+					<a class="nav__link" href="#">Services</a>
+					<a class="nav__link" href="#">Blog</a>
+					<a class="nav__link" href="#">Work</a>
+					<a class="nav__link" href="#">Contact</a>
+				</nav>
+			</div>
+		</div>	
+	</header>
+
+	<div class="intro">
+		<div class="container">
+			<div class="intro__inner">
+				<h2 class="intro__suptitle">Creative Template</h2>
+				<h1 class="intro__title">Welcome to MoGo</h1>
+
+				<a class="btn" href="#">Learn More</a>
+			</div>
+		</div>
+		<div class="slider">
+			<div class="container">
+				<div class="slider_inner">
+					<div class="slider_item active">
+						<span class="slider_num">01</span>
+						Intro
+					</div>
+					<div class="slider_item">
+						<span class="slider_num">02</span>
+						Work
+					</div>
+					<div class="slider_item">
+						<span class="slider_num">03</span>
+						About
+					</div>
+					<div class="slider_item">
+						<span class="slider_num">04</span>
+						Contacts
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
